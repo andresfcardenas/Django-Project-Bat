2c5e62d1e499261113255e1c78c91c6e23bd5909
Andres Cardenas <akardenasjimenez@gmail.com>
2011-12-03 17:58:09 -0500

    Minor changes on settings.py

--- projectoEnsayo/settings.py
+++ projectoEnsayo/settings.py
@@ -16,7 +16,7 @@ DATABASES = {
         'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'projectoEnsayo',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': 'covenant91afcj87spartan',                  # Not used with sqlite3.
+        'PASSWORD': '123456',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
